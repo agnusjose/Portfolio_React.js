8c98ad7f7a93b1375caddb90e64ace55edd5e011
agnusjose <agnus.au8@gmail.com>
2025-09-01 14:49:49 +0530

    Fixed profile photo issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import "./Home.css"
+import profilePic from '.src/assets/profile.jpg';
+
 function Home() {
   return (
     <div className="home">
       <div className="left">
-              <img src="src/assets/agnus2.jpg" alt="Profile photo" />
+        <img src={profilePic} alt="Profile" />  
       </div>
       <div className="right">
       <h2>Hi ! <br />I am <span style={{fontFamily:"monospace" , fontSize:"70px" , margin:"10px"}}>Agnus Jose</span></h2>
