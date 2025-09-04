0cd44c343256d206a61f09dfa7e205232ae541d1
agnusjose <agnus.au8@gmail.com>
2025-09-04 12:15:25 +0530

    update in image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import "./Home.css"
+import agnus2 from "../assets/agnus2.jpg"
 function Home() {
   return (
     <div className="home">
       <div className="left">
-        <img src="public/agnus2.jpg" alt="Profile" />
+        <img src={agnus2} alt="Profile" />
       </div>
       <div className="right">
       <h2>Hi ! <br />I am <span style={{fontFamily:"monospace" , fontSize:"70px" , margin:"10px"}}>Agnus Jose</span></h2>
